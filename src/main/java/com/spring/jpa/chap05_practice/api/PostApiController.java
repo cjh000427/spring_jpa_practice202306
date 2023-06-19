fa5b52d848d101b4db6ab2601a7f909823e614f3
jihyeok <kidjh@naver.com>
2023-06-19 18:19:56 +0900

    0619 서비스,컨트롤러

---
+++ src/main/java/com/spring/jpa/chap05_practice/api/PostApiController.java
@@ -0,0 +1,47 @@
+package com.spring.jpa.chap05_practice.api;
+
+import com.spring.jpa.chap05_practice.dto.PageDTO;
+import com.spring.jpa.chap05_practice.dto.PostListResponseDTO;
+import com.spring.jpa.chap05_practice.service.PostService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
+public class PostApiController {
+
+    // 리소스: 게시물 (Post)
+    /*
+        게시물 목록 조회: /posts            - GET
+        게시물 개별 조회: /posts/{id}       - GET
+        게시물 등록:     /posts            - POST
+        게시물 수정:     /posts/{id}       - PATCH
+        게시물 삭제:     /posts/{id}       - DELETE
+     */
+
+    //@Autowired 대신 @RequiredArgsConstructor 작성했기 때문에 가능
+    private final PostService postService;
+
+    @GetMapping
+    public ResponseEntity<?> List(PageDTO pageDTO) {
+        log.info("/api/v1/posts?page={}&size={}", pageDTO.getPage(), pageDTO.getSize());
+
+        PostListResponseDTO dto = postService.getPosts(pageDTO);
+
+        return ResponseEntity
+                .ok()
+                .body(dto);
+    }
+
+
+
+
+
+
+}
